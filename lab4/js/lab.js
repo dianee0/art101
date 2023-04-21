7a5290c49f1f09c3fb53827f5f0bce0a9bf061ba
Diane Poeng <95322564+dianee0@users.noreply.github.com>
2023-04-21 16:44:03 -0700

    Lab4 website finalized

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,15 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of this file is to provide psuedocode to create the game of rock, paper, scissors.
+// Author: Diane Poeng
+// Date: 4/21/23
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function Game(param1, param2) {
+
+  //Print images of 'rock' 'paper' and 'scissors' on the screen.
+  //Print a timer for each player (10 seconds).
+  //Timer starts counting down from 10 seconds:
+    //while seconds is greater than 0
+      //print the value of seconds
+      //subtract 1 from seconds 
+  //Accept user input from both ends.
+    //If no user input is obtained from both players
+      //the game restarts.
+  //If timer reaches 0
+    //print both user inputs on screen.
+  //Print the winner's username.
+  
+
 }
 
 function main() {
