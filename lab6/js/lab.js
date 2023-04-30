30c04c5fe42f222e1112ab847d0b407a0618ea80
Diane Poeng <95322564+dianee0@users.noreply.github.com>
2023-04-29 18:02:44 -0700

    creating lab6 website and updating main

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of this lab is to familarize ourselves with arrays and objects in JavaScript.
+// Author: Diane Poeng
+// Date: 4/27/23
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Variables
+let myTransport = ["bus", "walk", "car"];
+let myMainRide = {
+  make: "Hyundai", 
+  model: "Elantra",
+  color: "grey",
+  year: 2018,
+  age: function() {
+      return 2022 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// output
+document.writeln("Kinds of transportation I use: " + myTransport + ".</br>");
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
-// let's get this party started
-main();
